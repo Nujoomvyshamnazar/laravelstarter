8c77ce2b385b594e2ada43ebeaa03a521d1f8e08
Nujoomvyshamnazar <nujoomvyshamnazar@gmail.com>
2023-07-11 11:54:35 +0530

    initial commit with model,controller and view

---
+++ app/Http/Controllers/backend/SkillController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers\backend;
+
+use App\Http\Controllers\Controller;
+use App\Models\Skill;
+use Illuminate\Http\Request;
+
+class SkillController extends Controller
+{
+    public function index()
+    {
+        $skills = Skill::all();
+
+        return view('skills.index',compact('skills'));
+    }
+}
